cd37f3ead74a9b822e0c1ed77af4f26f6a931d9e
HemrajRijal <her0139@my.londonmet.ac.uk>
2018-03-08 07:37:43 +0545

    AOP pointcut declaration example

--- SpringDemoAOP/src/com/hemraj/demo/aopdemo/aspect/MyDemoLoggingAspect.java
+++ SpringDemoAOP/src/com/hemraj/demo/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,6 +2,7 @@ package com.hemraj.demo.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 /**
@@ -11,12 +12,23 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
+
+    //    Make a One point expression and re-use
+    @Pointcut("execution(* com.hemraj.demo.aopdemo.dao.*.*( ..))")
+    public void forDAOPackage() {
+    }
+
 //    This is where we add all of our related advices for logging
 
     //    Lets start with @Before annotation
 //    @Before("execution(public void add*( ..))")
-    @Before("execution(* com.hemraj.demo.aopdemo.dao.*.*( ..))")
+    @Before("forDAOPackage()")
     public void beforeAddAccountAdvice() {
         System.out.println("\n ======> EXecuting before Advice on addAccount");
+    }
+
+    @Before("forDAOPackage()")
+    public void performAPIAnalytics() {
+        System.out.println(" =====> Analysing API");
     }
 }
